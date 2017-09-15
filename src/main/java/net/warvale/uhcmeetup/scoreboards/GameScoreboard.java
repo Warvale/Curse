2cb1ec619db7ea00c0f140e77b909934347181c2
Henry <pixelificz@gmail.com>
2017-09-15 15:29:50 -0400

    Update GameScoreboard.java

--- src/main/java/net/warvale/uhcmeetup/scoreboards/GameScoreboard.java
+++ src/main/java/net/warvale/uhcmeetup/scoreboards/GameScoreboard.java
@@ -29,39 +29,39 @@ public class GameScoreboard {
             ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
             Scoreboard scoreboard = scoreboardManager.getNewScoreboard();
             Objective objective = scoreboard.registerNewObjective("game", "dummy");
-            objective.setDisplayName(ChatColor.DARK_GRAY + "» " + ChatColor.DARK_AQUA + "UHC Meetup" + ChatColor.DARK_GRAY + " «");
+            objective.setDisplayName(ChatColor.DARK_GRAY + "» " + ChatColor.RED + "UHC Meetup" + ChatColor.DARK_GRAY + " «");
             objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
             objective.getScore("  ").setScore(9);
             Team gameMode = scoreboard.registerNewTeam("GameMode");
-            gameMode.addEntry(ChatColor.GREEN + "Mode: ");
+            gameMode.addEntry(ChatColor.AQUA + "Mode: ");
             gameMode.setSuffix(ChatColor.WHITE + UHCMeetup.getGame().getType().toString());
-            objective.getScore(ChatColor.GREEN + "Mode: ").setScore(8);
+            objective.getScore(ChatColor.AQUA + "Mode: ").setScore(8);
 
             Team kills = scoreboard.registerNewTeam("PlayerKills");
-            kills.addEntry(ChatColor.GREEN + "Your Kills: ");
+            kills.addEntry(ChatColor.AQUA + "Your Kills: ");
             kills.setSuffix(ChatColor.WHITE + String.valueOf(PlayerManager.getInstance().getMeeupPlayer(player.getUniqueId()).getKills()));
-            objective.getScore(ChatColor.GREEN + "Your Kills: ").setScore(7);
+            objective.getScore(ChatColor.AQUA + "Your Kills: ").setScore(7);
 
 
             Team online = scoreboard.registerNewTeam("PlayersLeft");
-            online.addEntry(ChatColor.GREEN + "Players Left: ");
+            online.addEntry(ChatColor.AQUA + "Players Left: ");
             online.setSuffix(ChatColor.WHITE + String.valueOf(PlayerManager.getInstance().getAlivePlayers().size()));
-            objective.getScore(ChatColor.GREEN + "Players Left: ").setScore(6);
+            objective.getScore(ChatColor.AQUA + "Players Left: ").setScore(6);
 
             Team specs = scoreboard.registerNewTeam("SpecCount");
-            specs.addEntry(ChatColor.GREEN + "Spectators: ");
+            specs.addEntry(ChatColor.AQUA + "Spectators: ");
             specs.setSuffix(ChatColor.WHITE + String.valueOf(PlayerManager.getInstance().getSpectators().size()));
-            objective.getScore(ChatColor.GREEN + "Spectators: ").setScore(5);
+            objective.getScore(ChatColor.AQUA + "Spectators: ").setScore(5);
 
             objective.getScore("  ").setScore(4);
             Team border = scoreboard.registerNewTeam("BorderSize");
-            border.addEntry(ChatColor.GREEN + "Border: ");
+            border.addEntry(ChatColor.AQUA + "Border: ");
             border.setSuffix(ChatColor.WHITE + String.valueOf(UHCMeetup.getGame().getCurrentBorder()));
-            objective.getScore(ChatColor.GREEN + "Border: ").setScore(3);
+            objective.getScore(ChatColor.AQUA + "Border: ").setScore(3);
 
             objective.getScore(" ").setScore(2);
-            objective.getScore(ChatColor.GREEN + "warvale.net").setScore(1);
+            objective.getScore(ChatColor.RED + "warvale.net").setScore(1);
 
             this.scoreboards.put(player, scoreboard);
             player.setScoreboard(this.scoreboards.get(player));
